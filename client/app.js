53df67f7dca34820f4135645cc86b60f9751fae9
RamiNawahda <rami.nawahda@gmail.com>
2020-11-03 20:10:16 +0200

    still need a lot of works

--- client/app.js
+++ client/app.js
@@ -1,14 +1,22 @@
-// var jsonText = document.getElementById('jsonText');
-// console.log(jsonText, 'ssssss', typeof jsonText)
-let convertJsonToCSV = (obj) => {
-    // var pObj=JSON.parse(obj);
-    var result = '';
-    result += Object.keys(obj).slice(0, -1).join(',');
-    result += "\n";
-    var valRec = function () {
-        result+=Object.values(obj).slice(0,-1).join(',');
-    }
-    valRec(obj);
+// // var jsonText = document.getElementById('jsonText');
+// // console.log(jsonText, 'ssssss', typeof jsonText)
+// let jtocsv = (obj) => {
+//     // var pObj=JSON.parse(obj);
+//     var result = '';
+//     result += Object.keys(obj).slice(0, -1).join(',');
+//     result += "\n";
+//     var valRec = function () {
+//         result+=Object.values(obj).slice(0,-1).join(',');
+//         // but if we have nested object or children has keys and values.
+//         if(typeof obj.children==='object'){
+//             for(var i=0;i<obj.length;i++){
+//                 valRec(obj[i]);
+//             }
+//         }
+//     }
+//     valRec(obj);
 
-    return result;
-}
+//     return result;
+// }
+
+// module.exports.jtocsv  = jtocsv;
--- server.js
+++ server.js
@@ -1,19 +1,41 @@
-const { json } = require('body-parser')
-const { response } = require('express')
 const express = require('express')
+const bodyparser = require('body-parser')
 const app = express()
 const port = 3000
+// var jtocsv = require('./client/app.js')
 
 app.use(express.static('client'))
+app.use(express.json());
+app.use(bodyparser.urlencoded());
 
 app.get('/index.html', (req, res) => {
   res.sendFile(__dirname+'/'+'index.html');
 })
-app.get('/convertJ',(req,res)=>{
-   console.log(req.query.jsonText)
-    res.end(JSON.stringify(req.query.jsonText));
+app.post('/convertJ',(req,res)=>{
+  //  console.log("post request",req.query.data)
+  //  res.end(jtocsv({a:1}))
+  res.end(jtocsv(req.body.data))
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+let jtocsv = (obj) => {
+  // var pObj=JSON.parse(obj);
+  var result = '';
+  result += Object.keys(obj).slice(0, -1).join(',');
+  result += "\n";
+  var valRec = function () {
+      result+=Object.values(obj).slice(0,-1).join(',');
+      // but if we have nested object or children has keys and values.
+      if(typeof obj.children==='object'){
+          for(var i=0;i<obj.length;i++){
+              valRec(obj[i]);
+          }
+      }
+  }
+  valRec(obj);
+
+  return result;
+}
