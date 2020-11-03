b79b2484abc311d348d2e87f8ad946ec614d6fca
RamiNawahda <rami.nawahda@gmail.com>
2020-11-03 19:09:47 +0200

    things occurred

--- client/app.js
+++ client/app.js
@@ -1,4 +1,14 @@
-var jsonText=document.getElementById('jsonText').innerText;
-let convertJsonToCSV =  function (jsonText){
-    return jsonText;
+// var jsonText = document.getElementById('jsonText');
+// console.log(jsonText, 'ssssss', typeof jsonText)
+let convertJsonToCSV = (obj) => {
+    // var pObj=JSON.parse(obj);
+    var result = '';
+    result += Object.keys(obj).slice(0, -1).join(',');
+    result += "\n";
+    var valRec = function () {
+        result+=Object.values(obj).slice(0,-1).join(',');
+    }
+    valRec(obj);
+
+    return result;
 }
